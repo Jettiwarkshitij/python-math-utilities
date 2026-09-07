17aa8b7eedc4b988705889afa30898a73688b19e
kshitij jettiwar <kshitijjettiwar681@gmail.com>
2026-09-07 14:01:17 +0530

    Add files via upload

---
+++ practical5pg2.py
@@ -0,0 +1,6 @@
+#Program 2: Functional Programming Module
+
+from functools import reduce
+numbers = [1, 2, 3, 4, 5]
+result = reduce(lambda x, y: x + y, numbers)
+print("Sum using reduce:", result)
