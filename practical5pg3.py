693921c861c791c37a1e5aa1d6d7fafc688b47d8
kshitij jettiwar <kshitijjettiwar681@gmail.com>
2026-09-07 14:00:22 +0530

    Add files via upload

---
+++ practical5pg3.py
@@ -0,0 +1,14 @@
+#Program 3: User-defined Module Concept
+
+#Step 1: Create file my_module.py
+
+def add(a, b):
+ return a + b
+def multiply(a, b):
+ return a * b
+ 
+#Step 2: Import and use module
+
+import my_module
+print("Addition:", my_module.add(5, 3))
+print("Multiplication:", my_module.multiply(4, 2))
